e5d0b4b0abdc6cca6c71a3379489f4dff622d216
Asmaa Mohamed <engineerasmaa01@gmail.com>
2020-10-08 13:25:13 +0200

    listAll-products

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import PRODUCTS from './components/products';
+import store from './store';
 
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
+  render(){
   return (
+    <Provider store={store}>
     <div className="grid-container">
       <header>
         <a href="/" >Products</a>
       </header>
       <main>
-        product list
+        <div className="content">
+          <div className="main">
+            <PRODUCTS products={this.state.products}></PRODUCTS>
+          </div>
+          <div className="sidebar">
+            
+          </div>
+        </div>
       </main>
       <footer>
         All rights reserved
       </footer>
     </div>
+    </Provider>
   );
+  }
 }
 
 export default App;
---
+++ src/components/products.js
@@ -0,0 +1,81 @@
+import { connect } from 'react-redux';
+import React  from 'react';
+import { fetchProducts } from '../actions/productsAction';
+//import PropTypes from 'prop-types';
+
+
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+// const propTypes = {};
+
+// const defaultProps = {};
+
+/**
+ * 
+ */
+class products extends React.Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+        product:null
+    };
+}
+componentDidMount(){
+    this.props.fetchProducts();
+    console.log(this.props.fetchProducts());
+}
+
+    render() {
+        return (<div>
+          {
+          !this.props.products ?( <div>..loading</div>):
+          
+            (<ul className="products">
+              {this.props.products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
+                    <a
+                      href={"#" + product._id}
+                    >
+                      <img src={product.image} alt={product.title}></img>
+                      <p>{product.name} ({product.category})</p>
+                      <p>Available : { product.availableQuantity}</p>
+                    </a>
+                    <div className="product-price">
+                      <div>{product.price} $</div>
+                      <button
+                        onClick={() => this.props.addToCart(product)}
+                        className="button primary"
+                      >
+                        Add To Cart
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            )}
+        </div>
+        );
+    }
+}
+
+// products.propTypes = propTypes;
+// products.defaultProps = defaultProps;
+// // #endregion
+
+export default connect((state)=>({products:state.products.items}),{fetchProducts})
+(products);
