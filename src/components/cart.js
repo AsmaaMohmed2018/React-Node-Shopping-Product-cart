ad82f78e8af6d2205f1c3ad0b06f0e964342bf8b
Asmaa Mohamed <engineerasmaa01@gmail.com>
2020-10-10 11:04:42 +0200

    cart_items + total

--- src/components/cart.js
+++ src/components/cart.js
@@ -36,35 +36,66 @@ constructor(props) {
 }
     
     render() {
-        const {cartItems}= this.props;
-        return (<div className="cart">
-          <ul className="cart-items">
-            {cartItems.map((item) => (
-              <li key={item._id}>
-                <div>
-                  <img src={item.image} alt={item.title}></img>
-                </div>
-                <div>
-                  <div>{item.title}</div>
-                  <div className="right">
-                    {item.price} x {item.count}{" "}
-                    <button
-                      className="button"
-                      onClick={() => this.props.removeFromCart(item)}
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+      const { cartItems} = this.props;
+    return (
     
-      </div>)
+    <div >
+    {cartItems.length === 0 ? (
+          <div className="cart cart-header">Cart is empty</div>
+        ) : (
+          <div className="cart cart-header">
+            You have {cartItems.length} items in the cart{" "}
+          </div>
+    )}
+    
+    <ul className="cart-items">
+      {cartItems.map((item) => (
+        <li key={item._id}>
+          <div>
+            <img src={item.image} alt={item.title}></img>
+          </div>
+          <div>
+            <div>{item.title}</div>
+            <div className="right">
+              {item.price}$ x {item.count}{" "}
+              <button
+                className="button"
+                onClick={() => this.props.removeFromCart(item)}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+   
+    <div className="total">
+      <div>
+        Total:{" "}
+          {cartItems.reduce((a, c) => a + c.price * c.count, 0)} {"$"}
+      </div>
+      <button
+        onClick={() => {
+          this.setState({ showCheckout: true });
+        }}
+        className="button primary"
+      >
+        place order
+      </button>
+    </div>
+  </div>
+   
+
+    )
+    
     }
+  }
+       
 
+    
 
-}
+  
 // cart.propTypes = propTypes;
 // cart.defaultProps = defaultProps;
 // // #endregion
